b3e79f702168ec365190704c9768361999ee93e9
Umar Mohamed Usman <umarm@microsoft.com>
2020-02-24 23:10:30 -0800

    updated deploy.bat to remove @ from param

--- UmarDemoProj/Deploy/NORAD.Tracks.Santa.Functions/deploy.bat
+++ UmarDemoProj/Deploy/NORAD.Tracks.Santa.Functions/deploy.bat
@@ -1,1 +1,1 @@
-az group deployment create --resource-group noradsanta101-rg --template-file template.json --parameters @parameters.json
+az group deployment create --resource-group noradsanta101-rg --template-file template.json --parameters parameters.json
